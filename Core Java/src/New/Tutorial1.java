e4d016e1488401877aa6a98040bbefdcf2705acd
Anupam <anupammhr53@gmail.com>
2021-06-23 19:44:39 +0530

    committing eclipse core java repository1

---
+++ Core Java/src/New/Tutorial1.java
@@ -0,0 +1,50 @@
+package New;
+
+public class Tutorial1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//1 print hello world
+		System.out.println("hello world");
+		
+		//2 declaring variables of different data type (syntax is <datatype> variablename = data value;)
+		int a=23,x=34,y=55;
+		float n=23.45f;
+		boolean isabove18=false;
+		String d="anupam";
+		char b='s';
+	    System.out.println(a);
+	    System.out.println(x);
+	    System.out.println(y);
+	    System.out.println(n);
+		System.out.println(isabove18);
+		System.out.println(d);
+		System.out.println(b);
+		
+	}
+	
+	
+	
+
+}
+
+
+     
+
+    /* Rules for constructing name of variables in Java
+        1. Can contain digits, underscores, dollar signs, letters
+        2. Should begin with a letter, $ or _
+        3. Java is case sensitive language which means that
+           harry and Harry are two different variables altogether.
+        4. Should not contain whitespaces
+        5. You cannot use reserved keywords from Java
+     */
+    /*
+    Two types of Java Data Types:
+        1. Primitive - byte (1 byte), short (2 bytes), int(4 bytes), long (8 bytes), float(4 bytes),
+           double (8 bytes), boolean(1 bit), char (2 bytes). ( predefined in programming language )
+        2. Non Primitive or Reference Data Type - String, Class, Interface,Arrays ( not predefined )
+    */
+    
+
